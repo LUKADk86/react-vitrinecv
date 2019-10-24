971fd7c5715e1f4b63e88c5f02b5b86a71652831
LUKADk86 <lounis.86@gmail.com>
2019-10-24 23:14:12 +0200

    portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Portfolios, Portfoliotitle, Span, Overlay,Overlayspan, Img, Item, List, Box} from './style'
+import Axios from 'axios';
+
 
 
 
 const Portfolio=()=> {
+ const [images, setImages] = useState([]);
+ useEffect(() => {
+   Axios.get('js/data.json').then((res)=>{
+     setImages(res.data.portfolio)
+   });
+  
+ }, [])
+ const portoimages= images.map((item)=>{
+  return(
+    <Box key={item.id}>
+    <Img src={item.image}alt=""/>
+    <Overlay>
+        <Overlayspan>
+            Show Image
+        </Overlayspan>
+    </Overlay>
+</Box>
+  );
+})
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <Portfolios>
+
+      <Portfoliotitle><Span>My </Span>Portfolio</Portfoliotitle>
+      <List>
+          <Item>All</Item>
+          <Item>HTML</Item>
+          <Item>Photoshop</Item>
+          <Item>Wordpress</Item>
+          <Item>Mobile</Item>
+      </List>
+      
+      <div>
+          
+          {portoimages}
+          
+      </div>
+    
+</Portfolios>
   );
 }
 
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import {Works, Workpart, Parttitle, Partdesc, Line, Workparticon} from './style';
+import axios from 'axios'
 
 
+class Work extends React.Component {
+  state={
+    workship:[]
+  }
 
-const Work=()=> {
+
+componentDidMount(){
+  axios.get('js/data.json')
+  .then(res=>this.setState({workship: res.data.works}))
+}
+
+
+  render(){
+    const {workship}=this.state;
+    const workList=workship.map((workitem)=>{
+      return(
+        <Workpart first={workitem.id} key={workitem.id} >
+        <Workparticon className={workitem.icon_name}></Workparticon>
+        <Parttitle>{workitem.title}</Parttitle>
+        <Line/>
+        <Partdesc>
+            {workitem.body}
+        </Partdesc>
+    </Workpart>
+      );
+    })
+   
   return (
-    <div>
-      <h1>Work</h1>
-    </div>
+    <Works>
+    
+            <div className="container">
+              {workList}
+            </div>
+              
+     </Works>
+               
   );
+}
 }
 
 export default Work;
